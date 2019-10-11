66601cab9e7b82622f15f635fe1fa7da686b8ba6
saurabh8522 <saurabhsinghal.171co240@nitk.edu.in>
2019-10-11 13:20:22 +0530

    project complete

--- socialsite/socialsite/settings.py
+++ socialsite/socialsite/settings.py
@@ -122,4 +122,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIR= [STATIC_DIR,]
+STATICFILES_DIRS= [STATIC_DIR,]
+LOGIN_REDIRECT_URL = 'test'
+LOGOUT_REDIRECT_URL = 'thanks'
